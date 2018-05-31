eb0384793fdb65b18a63d846524c00aa6746847b
William Harding <william.jeffrey.harding@gmail.com>
2018-05-31 14:40:10 -0700

    First commit

---
+++ backend/FlaskAPIRoutes.py
@@ -0,0 +1,29 @@
+import yaml
+from flask import Blueprint, render_template,request
+from wtforms import Form, TextField, validators
+#import BOTO_BL as bbto
+
+adhocTestOne = Blueprint('adhocTestOne', __name__, template_folder='templates')
+APIroutes = [adhocTestOne]
+
+@adhocTestOne.route('/adhocTestOne')
+def adhocktestroute():
+	c = request.args.get('c')
+	t = request.args.get('t')
+	if t == "json":
+		blogTemplate = bbto.getBlogArticle(c)
+		return render_template('blogs/blog_template_json.html',blogContent=blogTemplate,article=c)
+	if t == "html":
+		try:
+			blogTemplate = bbto.getJupyterBlog(c)
+		except:
+			try:
+				c = c.replace(" ","+")
+				blogTemplate = bbto.getJupyterBlog(c)
+			except:
+				return c + " is an invalid blog article."
+		article = c.replace("blog/","")
+		article = article.replace(".html","")
+		return render_template('blogs/blog_template_jpyter.html',blogContent=blogTemplate,article=article)
+	return "invalid blog type (t)"
+
