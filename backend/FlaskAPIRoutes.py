55320db039729fbabc31eba2c8fe1484c4145713
William Harding <william.jeffrey.harding@gmail.com>
2018-06-04 17:16:18 -0700

    testing cookie cert as credential

--- backend/FlaskAPIRoutes.py
+++ backend/FlaskAPIRoutes.py
@@ -1,14 +1,16 @@
 import yaml
 from flask import Blueprint, render_template,request
 from wtforms import Form, TextField, validators
-#import BOTO_BL as bbto
 
 adhocTestOne = Blueprint('adhocTestOne', __name__, template_folder='templates')
 APIroutes = [adhocTestOne]
 
 @adhocTestOne.route('/adhocTestOne')
 def adhocktestroute():
-	c = request.args.get('c')
-	t = request.args.get('t')
-	return "The arguments are c:" + str(c) + " and t:" + str(t) 
+	if "UserID" in request.cookies:
+		if request.cookies["UserID"] == "testCookie":
+			return "User Accepted"
+		else: return (str(request.cookies))
+	else:
+		return "This user is not accepted"
 
